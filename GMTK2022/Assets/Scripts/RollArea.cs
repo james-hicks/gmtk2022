a8f81779ff53708eacf22dc4a946f27510f5069b
Andy Vu <minhvu007@gmail.com>
2022-07-17 04:00:11 -0700

    Damage number test

--- GMTK2022/Assets/Scripts/RollArea.cs
+++ GMTK2022/Assets/Scripts/RollArea.cs
@@ -2,12 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using DamageNumbersPro;
 
 public class RollArea : MonoBehaviour
 {
     [SerializeField] private float _requiredNumber;
     [SerializeField] private GameObject _newCam;
     [SerializeField] private GameObject _BumpZone;
+    public DamageNumber numberDmgPrefab;
+    public DamageNumber numberCritPrefab;
+
 
     public UnityEvent OnCorrectRoll;
 
@@ -48,28 +52,33 @@ public class RollArea : MonoBehaviour
         {
             Debug.Log("Critical Roll!");
             OnCorrectRoll.Invoke();
+            DamageNumber damageNumber = numberCritPrefab.Spawn(transform.position);
             _doOnce = true;
 
         } else if(_requiredNumber == 6 && _playerMovement.CurrentSide == 8)
         {
             Debug.Log("Critical Roll!");
             OnCorrectRoll.Invoke();
+            DamageNumber damageNumber = numberCritPrefab.Spawn(transform.position);
             _doOnce = true;
         } else if (_requiredNumber == 8 && _playerMovement.CurrentSide == 10)
         {
             Debug.Log("Critical Roll!");
             OnCorrectRoll.Invoke();
+            DamageNumber damageNumber = numberCritPrefab.Spawn(transform.position);
             _doOnce = true;
         } else if(_requiredNumber == 20 && _playerMovement.CurrentSide == 20)
         {
             Debug.Log("Critical Roll!");
             OnCorrectRoll.Invoke();
+            DamageNumber damageNumber = numberCritPrefab.Spawn(transform.position);
             _doOnce = true;
         }
         else if (_playerMovement.CurrentSide >= _requiredNumber)
         {
             _doOnce = true;
             OnCorrectRoll.Invoke();
+            DamageNumber damageNumber = numberDmgPrefab.Spawn(transform.position,_playerMovement.CurrentSide);
             Debug.Log("YOU GOT THE RIGHT ROLL! :DDDDDDDDDDDDDDDDDDDDD");
         }
     }
