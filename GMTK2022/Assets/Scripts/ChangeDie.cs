1a901cf25959c97e5165b634f64a10ea2cfa91df
James Hicks <james8hicks@gmail.com>
2022-07-17 02:13:15 -0700

    Sounds added to changing dice

--- GMTK2022/Assets/Scripts/ChangeDie.cs
+++ GMTK2022/Assets/Scripts/ChangeDie.cs
@@ -2,17 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
+using FMODUnity;
 
 public class ChangeDie : MonoBehaviour
 {
     [SerializeField] private GameObject _currentDice;
     [SerializeField] private GameObject _newDice;
+    [SerializeField] private FMODUnity.StudioEventEmitter eventEmitter;
 
     [SerializeField] private CinemachineFreeLook _playerCam;
     private void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Player")
         {
+            if (eventEmitter != null) eventEmitter.SendMessage("Play");
             this.gameObject.SetActive(false);
             _newDice.transform.position = _currentDice.transform.position;
             _playerCam.LookAt = _newDice.transform;
